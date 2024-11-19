834e6c2acfe2ed379a16e93946b3bf9f561c9edf
adminkaran123 <34962339+adminkaran123@users.noreply.github.com>
2024-11-19 12:42:41 +0530

    final code update

--- index.js
+++ index.js
@@ -1,24 +1,70 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-
+const dotenv = require('dotenv');
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+dotenv.config();
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
 const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS_TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+app.get('/', async (req, res) => {
 
-// * Code for Route 1 goes here
+    const contacts = 'https://api.hubspot.com/crm/v3/objects/2-37038850?properties=name,price,year';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(contacts, { headers });
+        const data = resp.data.results;
+        res.render('homepage', { title: 'Homepage | Custom Objects',data }); 
+    } catch (error) {
+        console.error(error);
+    }
+    
+    
+   
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+// Route 2 - Form page for creating/updating custom objects
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', { 
+        title: 'Create/Update Custom Object',
+    });
+});
+
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+app.post('/update-cobj', async (req, res) => {
+    const update = {
+        properties: {
+            name: req.body.name,
+            price: req.body.price,
+            year: req.body.year
+        }
+    }
+
+    const updateContact = 'https://api.hubapi.com/crm/v3/objects/2-37038850';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try { 
+        await axios.post(updateContact, update, { headers });
+        res.redirect('/');
+    } catch(err) {
+        console.error(err);
+        res.status(500).send(err.message);
+    }
+});
 
 // * Code for Route 3 goes here
 
@@ -68,4 +114,4 @@ app.post('/update', async (req, res) => {
 
 
 // * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(3000, () => console.log('Listening on http://localhost:3000',PRIVATE_APP_ACCESS));
